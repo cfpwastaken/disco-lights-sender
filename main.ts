98ff9c96a3ac5b5831b91cf0bfb9269ce5fde054
cfp <44261356+cfpwastaken@users.noreply.github.com>
2021-08-03 20:47:37 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,11 @@
+let sound = 0
+radio.setGroup(1)
+basic.forever(function () {
+    sound = input.soundLevel()
+    radio.sendNumber(sound)
+    if (0 > 105) {
+    	
+    } else {
+    	
+    }
+})
