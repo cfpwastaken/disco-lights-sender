7f2e1aaa41ee33c61b4cd634aeeca882fb06655d
cfp <44261356+cfpwastaken@users.noreply.github.com>
2021-08-04 13:49:28 +0200

    Yes

--- main.ts
+++ main.ts
@@ -1,6 +1,35 @@
-let sound = 0
+let amount = 0
+let show = true
 radio.setGroup(1)
 basic.forever(function () {
-    sound = input.soundLevel()
+    let sound = input.soundLevel()
+    sound = sound - amount
     radio.sendNumber(sound)
+})
+basic.forever(() => {
+    if(show == true) {
+        led.plotBarGraph(amount, 255)
+    }
+})
+
+basic.forever(() => {
+    if(show == true) {
+        basic.pause(2000)
+        basic.clearScreen()
+        show = false
+    }
+})
+
+input.onButtonPressed(Button.A, () => {
+    show = true
+    amount = amount - 1
+})
+
+input.onButtonPressed(Button.B, () => {
+    show = true
+    amount = amount + 1
+})
+
+input.onButtonPressed(Button.AB, () => {
+    basic.showNumber(amount)
 })
